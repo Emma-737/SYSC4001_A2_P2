4c9fd3dcb23cac3387ac91eb9de0845f21f62598
Emma TB <145624013+Emma-737@users.noreply.github.com>
2025-10-27 14:50:17 -0400

    Create fork-code-2.c
    second part of part 2

---
+++ fork-code-2.c
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+//base code is from the textbook, page 119
+int main()
+{
+    pid_t pid;
+    int count = 0;
+
+    /* fork a child process */
+    pid = fork();
+    
+    printf("pid: %d\n", pid);
+
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed\n");
+        return 1;
+    }
+    else if (pid == 0) {
+        while(1){ /* child process */
+            count += 1;
+            printf("Cycle Number Child: %d\n", count);
+            sleep(1);
+        }
+    }
+    else { /* parent process */
+        /* parent will wait for the child to complete */
+        //wait(NULL);
+        while(1){
+        count += 1;
+        printf("Cycle Number Parent: %d\n", count);
+        sleep(1);
+    }
+    }
+
+
+    return 0;
+}
